c17a888cecbaf6865c831e54b89ce34962381714
herrwolf <webmasterp2m@gmail.com>
2017-08-13 16:16:52 -0700

    Clase 81

--- backend/views/js/gestorArticulos.js
+++ backend/views/js/gestorArticulos.js
@@ -96,7 +96,7 @@ $(".editarArticulo").click(function(){
 	introduccion = $("#"+idArticulo).children("p").html();
 	contenido = $("#"+idArticulo).children("input").val();	
 	
-	$("#"+idArticulo).html('<form method="post" enctype="multipart/form-data"><span><button class="btn btn-primary pull-right">Guardar</button></span><div id="editarImagen"><input style="display:none" type="file" id="subirNuevaFoto" class="btn btn-default" style="color:transparent"><div id="nuevaFoto"><span class="fa fa-times cambiarImagen"></span><img src="'+rutaImagen+'" class="img-thumbnail"></div></div><input type="text" value="'+titulo+'" name="editarTitulo"><textarea cols="30" rows="5" name="editarIntroduccion">'+introduccion+'</textarea><textarea name="editarContenido" id="editarContenido" cols="30" rows="10">'+contenido+'</textarea><hr></form>')
+	$("#"+idArticulo).html('<form method="post" enctype="multipart/form-data"><span><input type="submit" class="btn btn-primary pull-right" value="Guardar" style="width:10%; padding:5px 0; margin-top:4px"></span><div id="editarImagen"><input style="display:none" type="file" id="subirNuevaFoto" class="btn btn-default"><div id="nuevaFoto"><span class="fa fa-times cambiarImagen"></span><img src="'+rutaImagen+'" class="img-thumbnail"></div></div><input type="text" value="'+titulo+'" name="editarTitulo"><textarea cols="30" rows="5" name="editarIntroduccion">'+introduccion+'</textarea><textarea name="editarContenido" id="editarContenido" cols="30" rows="10">'+contenido+'</textarea><hr></form>')
 
 	$(".cambiarImagen").click(function(){
 
@@ -105,6 +105,76 @@ $(".editarArticulo").click(function(){
 		$("#subirNuevaFoto").show();
 		$("#subirNuevaFoto").css({"width":"90%"});
 		$("#nuevaFoto").html("");
+		$("#subirNuevaFoto").attr("name","editarImagen");
+		$("#subirNuevaFoto").attr("require",true);
+
+		$("#subirNuevaFoto").change(function(){
+
+			imagen = this.files[0];
+	
+			//Validando el tamaño de la imagen
+
+			imagenSize = imagen.size;
+
+			if (Number(imagenSize)>2000000){
+
+				$("#editarImagen").before('<div class="alert alert-warning alerta text-center">El archivo excede el tamaño permitido, 2MB</div>');
+			}
+			else{
+
+				$(".alerta").remove();
+			}
+
+			//Validar tipo de imagen
+
+			imagenType = imagen.type;
+
+			if (imagenType == "image/jpeg" || imagenType == "image/png"){
+
+				$(".alerta").remove();
+			}
+			else{
+
+				$("#editarImagen").before('<div class="alert alert-warning alerta text-center">El archivo debe ser en formato JPG o PNG</div>');
+			}
+
+			//Mostrar imagen con AJAX
+
+			if (Number(imagenSize)< 2000000 && imagenType == "image/jpeg" || imagenType == "image/png"){
+
+				var datos = new FormData;
+
+				datos.append("imagen", imagen);
+
+				$.ajax({
+
+					url: "views/ajax/gestorArticulos.php",
+					method: "POST",
+					data: datos,
+					cache: false,
+					contentType: false,
+					processData: false,
+					beforeSend: function(){
+						
+						$("#nuevaFoto").html('<img src="../views/images/status.gif" style="width:15%" id="status2">');
+					},
+					success: function(respuesta){
+						$("#status2").remove();
+
+						if (respuesta ==0){
+
+							$("#editarImagen").before('<div class="alert alert-warning alerta text-center">La imagen es inferior a 800px * 400px</div>');
+						}
+						else{
+
+							$("#nuevaFoto").html('<img src="'+respuesta.slice(6)+'" class="img-thumbnail">');
+						}	
+
+						
+					}
+				})
+			}
+		})
 	})
 })
 
